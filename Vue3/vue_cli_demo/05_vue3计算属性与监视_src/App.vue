c07e8757c94803da8dfc2c48f962b51bc2eb9916
AsunaCC <577431769@qq.com>
2021-12-11 12:32:17 +0800

    feat: 新增vue3计算属性与监视

---
+++ Vue3/vue_cli_demo/05_vue3计算属性与监视_src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: Yran
+ * @Date: 2021-11-20 15:34:45
+ * @LastEditors: Yran
+ * @LastEditTime: 2021-12-11 10:59:08
+-->
+<template>
+  <Demo msg="test" value="value"></Demo>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+import Demo from "./components/Demo.vue";
+// import { h } from "vue";
+export default {
+  name: "App",
+  components: { Demo },
+};
+</script>
+
+<style>
+</style>
